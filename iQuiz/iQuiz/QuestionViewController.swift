9e862e8453a96d5052972aabdfed88decf74565d
lourencogabe <gabriels211@hotmail.com>
2024-09-30 22:13:24 -0300

    :art: style: formating button

--- iQuiz/iQuiz/QuestionViewController.swift
+++ iQuiz/iQuiz/QuestionViewController.swift
@@ -11,13 +11,22 @@ class QuestionViewController: UIViewController {
 
     @IBOutlet weak var questionTitle: UILabel!
     
+    @IBOutlet var buttonCollection: [UIButton]!
+    
     @IBAction func answerButton(_ sender: UIButton) {
         print(sender.tag)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        layoutConfigure()
+    }
+    
+    func layoutConfigure(){
         navigationItem.hidesBackButton = true
+        
+        for button in buttonCollection{
+            button.layer.cornerRadius = 12
+        }
     }
 }
